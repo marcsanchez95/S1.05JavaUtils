f7582726aef670da628f29bd1dd65cba78352c51
Wrennandsix <marcsmoreno@hotmail.com>
2023-10-16 09:39:18 +0200

    n1ex02 change

---
+++ S1.05JavaUtils/src/n1ex02/N1ex02.java
@@ -0,0 +1,12 @@
+package n1ex02;
+
+public class N1ex02 {
+
+	public static void main(String[] args) {
+		ListDirectoryContents2.directorySorterRecursively("C:\\Users\\marcs\\OneDrive\\Escritorio\\Programasao\\javautilsPrueba");
+				
+	}
+
+}
+
+
