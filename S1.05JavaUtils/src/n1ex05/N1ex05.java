196b52c055db351899509bbd4acc4477e2b86e09
Wrennandsix <marcsmoreno@hotmail.com>
2023-10-23 10:45:14 +0200

    afegit comentaris de modificacio.

--- S1.05JavaUtils/src/n1ex05/N1ex05.java
+++ S1.05JavaUtils/src/n1ex05/N1ex05.java
@@ -18,35 +18,40 @@ public class N1ex05 {
 		String consoleOutput = ListDirectoryContents5.readTextFile(outputFilePath);
 		System.out.println(consoleOutput);
 		
-		//prova i implementacio de serializar i deserialitzar objectes.
-		SerExampleCar ferrari = new SerExampleCar("Ferrari","Huracan",500);	
-		//cambiar el directori per un valid per guardar el .ser.
+		// prova i implementacio de serializar i deserialitzar objectes.
+		SerExampleCar ferrari = new SerExampleCar("Ferrari", "Huracan", 500);
+		
+		// cambiar el directori per un valid per guardar el .ser.		
 		serializeObject(ferrari, "C:\\Users\\marcs\\git\\S1.05JavaUtils\\S1.05JavaUtils\\src\\n1ex05\\ferrari.ser");
-		//cambiar el directori per el utilitzat al serialitzar el objecte.
-		SerExampleCar deserializedFerrari = (SerExampleCar) deserializeObject("C:\\Users\\marcs\\git\\S1.05JavaUtils\\S1.05JavaUtils\\src\\n1ex05\\ferrari.ser");
-		System.out.println("Deserialized car: "+deserializedFerrari.getBrand() +" "+ deserializedFerrari.getModel());
-	
+		
+		// cambiar el directori per el utilitzat al serialitzar el objecte.
+		SerExampleCar deserializedFerrari = (SerExampleCar) deserializeObject(
+				"C:\\Users\\marcs\\git\\S1.05JavaUtils\\S1.05JavaUtils\\src\\n1ex05\\ferrari.ser");
+		System.out
+		.println("Deserialized car: " + deserializedFerrari.getBrand() + " " + deserializedFerrari.getModel());
+
 	}
-	   public static void serializeObject(Object object, String filePath) {		  
-	        try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(filePath))) {
-	            outputStream.writeObject(object);
-	            System.out.println("Object serialized and saved to: " + filePath);
-	            
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	        }
-	    }   
 
-		public static Object deserializeObject(String filePath) {
+	public static void serializeObject(Object object, String filePath) {
+		try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(filePath))) {
+			outputStream.writeObject(object);
+			System.out.println("Object serialized and saved to: " + filePath);
 
-			try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(filePath))) {
-				Object object = inputStream.readObject();
-				System.out.println("Object deserialized from: " + filePath);
-				return object;
-				
-			} catch (IOException | ClassNotFoundException e) {
-				e.printStackTrace();
-				return null;
-			}
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
+
+	public static Object deserializeObject(String filePath) {
+
+		try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(filePath))) {
+			Object object = inputStream.readObject();
+			System.out.println("Object deserialized from: " + filePath);
+			return object;
+
+		} catch (IOException | ClassNotFoundException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
